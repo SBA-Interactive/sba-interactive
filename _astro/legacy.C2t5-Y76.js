a7ff2c3bbf378205516b2c1dddebf85a7ea22872
Bartosz Zakrzewski <bartoszzakrzewski@mti.edu.pl>
2026-01-16 18:21:31 +0100

    Update 8

---
+++ _astro/legacy.C2t5-Y76.js
@@ -0,0 +1,1 @@
+import{U as a}from"./render.Dux-qmzg.js";a();
---
+++ _astro/svelte-component.CsWamALq.js
@@ -0,0 +1,1 @@
+import{o as s,q as c,v as m,E as i}from"./render.Dux-qmzg.js";import{B as p}from"./branches.BdtDokLB.js";function E(n,r,o){s&&c();var e=new p(n);m(()=>{var a=r()??null;e.ensure(a,a&&(t=>o(t,a)))},i)}export{E as c};
