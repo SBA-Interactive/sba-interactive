65a3a6d3f7ff50fe17eff8f700b4efb6071e7fc6
Bartosz Zakrzewski <bartoszzakrzewski@mti.edu.pl>
2026-02-06 17:08:36 +0100

    Update web workers for gtag and google tag manager

---
+++ _astro/svelte-component.BgpZ3zwF.js
@@ -0,0 +1,1 @@
+import{k as s,l as c,o as m,E as i}from"./render.BcWKkXdG.js";import{B as p}from"./branches.OIGucOrb.js";function l(n,r,o){s&&c();var e=new p(n);m(()=>{var a=r()??null;e.ensure(a,a&&(t=>o(t,a)))},i)}export{l as c};
