23f9c96544965dea3e9b4c3fde0a91c6743f41d2
Bartosz Zakrzewski <bartoszzakrzewski@mti.edu.pl>
2026-02-06 16:46:41 +0100

    Update Padding fixed (v2)

---
+++ _astro/legacy.BZ9lSaad.js
@@ -0,0 +1,1 @@
+import{Z as a}from"./render.mehcsBJh.js";a();
---
+++ _astro/svelte-component.BwgIWBBY.js
@@ -0,0 +1,1 @@
+import{k as s,l as c,o as m,E as i}from"./render.mehcsBJh.js";import{B as p}from"./branches.C-Xa9uVY.js";function l(n,r,o){s&&c();var e=new p(n);m(()=>{var a=r()??null;e.ensure(a,a&&(t=>o(t,a)))},i)}export{l as c};
